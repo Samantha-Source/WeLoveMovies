d1ecf08ab97257e9e3c159d8cf2dabe1a0828bc3
Samantha Brown <S.BrownEMS@gmail.com>
2022-12-22 11:58:46 -0500

    Migrations created
    Critics, movies, and theaters tables

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://tkqjagfv:L4nWDAeNsDAkLAl27xGIDRpmxYRe8-RK@isilo.db.elephantsql.com/tkqjagfv",
 } = process.env;
 
 module.exports = {
