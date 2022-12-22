35a120361349432015f6157f9b8c42c16d725e9d
Samantha Brown <S.BrownEMS@gmail.com>
2022-12-22 12:51:41 -0500

    Migrations/seeds
    Migrations and seeds completed for tables:
    critics, movies, theaters, reviews, movies

---
+++ src/db/migrations/04_createReviewsTable.js
@@ -0,0 +1,24 @@
+exports.up = function(knex){
+    return knex.schema.createTable("reviews", (table) =>{
+        table.increments("review_id").primary();
+        table.text("content");
+        table.integer("score");
+        table.integer("critic_id").unsigned().notNullable();
+        table
+            .foreign("critic_id")
+            .references("critic_id")
+            .inTable("critics")
+            .onDelete("cascade");
+        table.integer("movie_id").unsigned().notNullable();
+        table
+            .foreign("movie_id")
+            .references("movie_id")
+            .inTable("movies")
+            .onDelete("cascade");
+        table.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex){
+    return knex.schema.dropTable("reviews");
+};
