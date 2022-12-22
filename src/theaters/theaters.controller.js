86f0ba0253bca9bbd0df60292f20a4bfda2696b7
Samantha Brown <S.BrownEMS@gmail.com>
2022-12-22 16:37:54 -0500

    Theaters route completed

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,22 +1,13 @@
 const { response } = require("express");
 const service = require("./theaters.service");
-//const asyncErrorBoundary needed?????
 
 const reduceProperties = require("../utils/reduce-properties");
 
 
-
 async function list(req, res, next) {
   const data = await service.list()
-  console.log(JSON.stringify(data))
-  res.json({  data })
+  res.json({ data })
 };
-
-
-
-
-
-
 
 
 module.exports = {
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -2,46 +2,25 @@ const knex = require("../db/connection");
 const reduceProperties = require("../utils/reduce-properties");
 const mapProperties = require("../utils/map-properties");
 
-// const reduceTheaterAndMovies = reduceProperties("theater_id", {
-// //   theater_id: ["theater", "theater_id"],
-// //   name: ["theater", "name"],
-// //   address_line_1:["theater", "address_line_1"],
-// //   address_line_2:["theater", "address_line_2"],
-// //   city:["theater", "city"],
-// //   state:["theater", "state"],
-// //   zip:["theater", "zip"],
-// //   created_at:["theater", "created_at"],
-// //   updated_at:["theater", "updated_at"],
-  
-//   movie_id: ["movies", null, "movie_id"],
-//   title: ["movies", null, "title"],
-//   runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
-//   rating: ["movies", null, "rating"],
-//   description: ["movies", null, "description"],
-//   image_url:["movies", null, "image_url"],
-//   created_at:["movies", null, "created_at"],
-//   updated_at:["movies", null, "updated_at"],
-//   is_showing:["movies", null, "is_showing"],
-//   theater_id:["movies", null, "theater_id"]
-// })
-
 const reduceMovies = reduceProperties("theater_id", {
   movie_id: ["movies", null, "movie_id"],
-  title:["movies", null, "title"],
+  title: ["movies", null, "title"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
   rating: ["movies", null, "rating"],
-  runtime_in_minutes: ["movies", null, "runtime_in_minutes"]
+  description: ["movies", null, "description"],
+  image_url:["movies", null, "image_url"],
+  created_at:["movies", null, "created_at"],
+  updated_at:["movies", null, "updated_at"],
+  is_showing:["movies", null, "is_showing"],
+  theater_id:["movies", null, "theater_id"]
 })
+
 
 function list(){
   return knex("theaters")
    .join("movies_theaters", "theaters.theater_id", "movies_theaters.theater_id")
    .join("movies", "movies.movie_id", "movies_theaters.movie_id")
-//    .select("theaters.theater_id", "theaters.name", "theaters.address_line_1", "theaters.address_line_2", "theaters.city", "theaters.state", "theaters.zip", "theaters.created_at", "theaters.updated_at" "movies.movie_id", "movies.title", "movies.runtime_in_minutes", "movies.rating", "movies.description", "movies.image_url", "movies.created_at", "movies.updated_at", "movies.is_showing", "movies.theater_id")
-  .where({theater_id: theater_id})
   .select("*")
-  .groupBy("theaters.theater_id")
-//   .orderBy("theaters.theater_id")
-//   .then(reduceTheaterAndMovies)
   .then(reduceMovies)
 };
 
